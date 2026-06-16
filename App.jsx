184c1e87e3be68cacbf0cf70b7b8cf806bcb2e1c
janhavi-code2748 <janhavichoudhary2748@gmail.com>
2026-06-16 16:47:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>Counter App</h1>
+
+      <h2>{count}</h2>
+
+      <div className="buttons">
+        <button onClick={() => setCount(count + 1)}>
+          Increase
+        </button>
+
+        <button onClick={() => setCount(count - 1)}>
+          Decrease
+        </button>
+
+        <button onClick={() => setCount(0)}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
